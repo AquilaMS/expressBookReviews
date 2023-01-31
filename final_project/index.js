fdf0527d2040304ce4c4d985843007a96c745b60
Áquila Mazzei <aquila.botafogo@gmail.com>
2023-01-31 16:56:54 -0300

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,27 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+ const username = req.body.username
+ const password =req.body.password
+
+  if (!password || !username){
+      return res.status(401).json({error: 'Missing fields'})
+  } 
+
+  if(authenticatedUser(username, password)){
+      const token = jwt.sign({
+          data: password
+      }, 'access', { expiresIn: 60 * 60 })
+
+      req.session.authorization = {
+        token, username
+    }
+    return res.status(200).send('Logged')
+  }
+  else{
+    return res.status(208).json({error: "Invalid Login. Check username and password"});
+  }
+
 });
  
 const PORT =5000;
