b48537dd67e2f483a128d51dc01789d2097c8c66
Áquila Mazzei <aquila.botafogo@gmail.com>
2023-01-31 22:48:56 -0300

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,19 +3,27 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [{username:'aquila', password:'123456'}];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
+    const checkUser = users.filter(item=>item.username == username && item.password == password)
+    if(checkUser.length ==1 ){
+        return true
+    }
+    return false
 //write code to check if username and password match the one we have in records.
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
+regd_users.post("/", (req,res) => {
   //Write your code here
+  if(authenticatedUser(req.body.username, req.body.password)){
+    return res.status(200).json({username:req.body.username, password:req.body.password});
+  }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
@@ -28,3 +36,4 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
+module.exports.regd_users = regd_users; 
