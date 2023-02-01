76faa26f8850fb779ee3686ded98e935ccce0463
Áquila Mazzei <aquila.botafogo@gmail.com>
2023-02-01 13:19:24 -0300

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,57 +6,101 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req, res) => {
-    if (!req.body.username) return res.send({ error: 'Insert a username' })
-    if (!req.body.password) return res.send({ error: 'Insert a password' })
-    const username = req.body.username
-    const usersCount = users.filter(item => item.username == username)
-    if (usersCount.length >= 1) {
-        console.log(req.body)
-        return res.send({ error: 'User already registered' })
-    }
-    const insertedUser = users.push({ username: req.body.username, password: req.body.password })
-    return res.status(200).json(insertedUser);
+  if (!req.body.username) return res.send({ error: 'Insert a username' })
+  if (!req.body.password) return res.send({ error: 'Insert a password' })
+
+  const username = req.body.username
+  const usersCount = users.filter(item => item.username == username)
+  if (usersCount.length >= 1) {
+    console.log(req.body)
+    return res.send({ error: 'User already registered' })
+  }
+  const insertedUser = users.push({ username: req.body.username, password: req.body.password })
+  return res.status(200).json(insertedUser);
 });
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-    return res.status(200).json({ data: books })
+  return res.status(200).json({ data: books })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-    const filtereredBook = books[req.params.isbn]
-    return res.status(200).json(filtereredBook);
+  const filtereredBook = new Promise((resolve, rejected) => {
+    try {
+      const data = books[req.params.isbn]
+      resolve(data)
+    }
+    catch (err) {
+      rejected(err)
+    }
+  })
+  const result = filtereredBook.then((result) => {
+    return res.status(200).json(result);
+  })
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-    const booksLength = Object.keys(books).length
-    var filteredByAuthor = []
-    for (i = 1; i <= booksLength; i++) {
+  const booksByAuthor = new Promise((resolve, rejected) => {
+    try {
+      const booksLength = Object.keys(books).length
+      var filteredByAuthor = []
+      for (i = 1; i <= booksLength; i++) {
         if (books[i].author == req.params.author) {
-            filteredByAuthor.push(books[i])
+          filteredByAuthor.push(books[i])
         }
+      }
+      console.log(filteredByAuthor)
+      resolve(res.status(200).json(filteredByAuthor))
     }
-    return res.status(200).json(filteredByAuthor);
+    catch (err) {
+      rejected(err)
+    }
+  })
+  booksByAuthor.then(result => {
+    return result
+  })
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-    const booksLength = Object.keys(books).length
-    var filteredByTitle = []
-    for (i = 1; i <= booksLength; i++) {
+  const booksByTitle = new Promise((resolve, rejected) => {
+    try {
+      const booksLength = Object.keys(books).length
+      var filteredByTitle = []
+      for (i = 1; i <= booksLength; i++) {
         if (books[i].title == req.params.title) {
-            filteredByTitle.push(books[i])
+          filteredByTitle.push(books[i])
         }
+      }
+      console.log(filteredByTitle)
+      resolve(res.status(200).json(filteredByTitle))
     }
-    return res.status(200).json(filteredByTitle);
+    catch (err) {
+      rejected(err)
+    }
+  })
+  booksByTitle.then(result => {
+    return result
+  })
 });
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-    const filtereredReviews = books[req.params.isbn].reviews
-    return res.status(200).json(filtereredReviews);
+  const reviewByISBN = new Promise((resolve, rejected) => {
+    try {
+      const filtereredReviews = books[req.params.isbn].reviews
+      resolve(res.status(200).json(filtereredReviews))
+
+    }
+    catch (err) {
+      rejected(err)
+    }
+  })
+  reviewByISBN.then(result => {
+    return result
+  })
 });
 
 module.exports.general = public_users;
