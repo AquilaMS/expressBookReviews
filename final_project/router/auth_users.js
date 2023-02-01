68a1af56e696f2b0e7a7c694582590deb9e3e15c
Áquila Mazzei <aquila.botafogo@gmail.com>
2023-01-31 23:01:03 -0300

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -4,6 +4,7 @@ let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [{username:'aquila', password:'123456'}];
+const SECRET = 'supersecret'
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
@@ -15,20 +16,21 @@ const authenticatedUser = (username,password)=>{ //returns boolean
         return true
     }
     return false
-//write code to check if username and password match the one we have in records.
 }
 
 //only registered users can login
 regd_users.post("/", (req,res) => {
   //Write your code here
   if(authenticatedUser(req.body.username, req.body.password)){
-    return res.status(200).json({username:req.body.username, password:req.body.password});
+    return res.send({
+        token: jwt.sign({username: req.body.username}, SECRET)
+    })
   }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
+regd_users.put("/auth/review/:isbn",  (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
